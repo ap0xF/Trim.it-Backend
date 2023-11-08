5c7fc2cb80f77b956bcb911f4393b590639948dd
ap0xF <contact.ap0xf@gmail.com>
2023-11-08 13:39:52 +0545

    @Get and redirection

--- src/main/java/com/syano/controller/UrlController.java
+++ src/main/java/com/syano/controller/UrlController.java
@@ -1,19 +1,27 @@
 package com.syano.controller;
-
-import com.syano.model.UrlEntity;
-import com.syano.repository.UrlRepository;
-import io.micronaut.data.annotation.GeneratedValue;
+import com.syano.GetUrlRequest;
+import com.syano.service.UrlService;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import jakarta.inject.Inject;
-import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
+import java.net.URI;
 
 @Controller("/")
 public class UrlController {
-    @Inject
-    private UrlRepository urlRepository;
 
+    @Inject
+    private UrlService urlService;
+    private static final Logger LOG = LoggerFactory.getLogger(UrlController.class);
+    @Get("/{path}")
+    public HttpResponse<?> redirect(String path) {
+        String longUrl = urlService.getLongUrl(path);
+        System.out.println(longUrl);
+        URI uri = URI.create(longUrl);
+        return HttpResponse.redirect(uri);
+    }
 }
