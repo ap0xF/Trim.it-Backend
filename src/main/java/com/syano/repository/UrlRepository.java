5ced1c9600e3ee29a694260c1baceeea17a7d97d
ap0xF <contact.ap0xf@gmail.com>
2023-11-08 13:39:20 +0545

    nothing

--- src/main/java/com/syano/repository/UrlRepository.java
+++ src/main/java/com/syano/repository/UrlRepository.java
@@ -1,6 +1,7 @@
 package com.syano.repository;
 
 import com.syano.model.UrlEntity;
+import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jpa.repository.JpaRepository;
 import io.micronaut.data.mongodb.annotation.MongoRepository;
 
@@ -10,11 +11,8 @@ import java.util.Optional;
 
 @MongoRepository
 public interface UrlRepository extends JpaRepository<UrlEntity, ObjectId> {
-    public Optional<UrlEntity> findById(ObjectId id); // because findById expects
-    // data type of Integer and ObjectId is a 12 bit data type used in mongodb.
-
-    public UrlEntity findByShortUrl(String shortUrl);
-
-    public void deleteByShortUrl(String shortUrl);
-
+    Optional<UrlEntity> findById(ObjectId id); // because findById expects
+    // data type of Integer and ObjectId is a 12 bit data type used in mongodb.  //need more research on this.
+    UrlEntity findByShortUrl(String shortUrl);
+    void deleteByShortUrl(String shortUrl);
 }
