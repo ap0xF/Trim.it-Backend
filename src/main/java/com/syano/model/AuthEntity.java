fa80d0214ee8a4b99c072ddc8e11f3d431a405fc
ap0xF <contact.ap0xf@gmail.com>
2023-11-22 17:10:19 +0545

    Entity for login/signup

---
+++ src/main/java/com/syano/model/AuthEntity.java
@@ -0,0 +1,59 @@
+package com.syano.model;
+
+import com.google.protobuf.ByteString;
+import io.micronaut.data.annotation.Id;
+import io.micronaut.data.annotation.MappedEntity;
+import jakarta.inject.Singleton;
+import org.bson.types.ObjectId;
+
+@MappedEntity
+public class AuthEntity {
+    @Id
+    private ObjectId _id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+
+    private String password;
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
