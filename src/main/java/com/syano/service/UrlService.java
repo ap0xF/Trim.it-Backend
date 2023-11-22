cbe9905b3400a6992345f4b63a54602ce07257f8
ap0xF <contact.ap0xf@gmail.com>
2023-11-22 17:17:32 +0545

    updated deleteUrl and added getAllUrlsOfUser

--- src/main/java/com/syano/service/UrlService.java
+++ src/main/java/com/syano/service/UrlService.java
@@ -9,11 +9,9 @@ import io.micronaut.data.exceptions.EmptyResultException;
 import io.seruco.encoding.base62.Base62;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.validation.constraints.NotNull;
-import org.bson.types.ObjectId;
-
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.List;
 import java.util.Objects;
 
 @Singleton
@@ -21,6 +19,7 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
 
     @Inject
     private UrlRepository urlRepository;
+
 
     @Override
     public void createUrl(CreateUrlRequest createUrlRequest, StreamObserver<CreateUrlResponse> responseObserver) {
@@ -30,11 +29,11 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
         if(!longUrl.isEmpty()){
             Entity entity = Entity.newBuilder()
                     .setLongUrl(longUrl)
-//                .setExpirationTime(createUrlRequest.getExpirationTime())
+                    .setEmail(createUrlRequest.getEmail())
                     .build();
             UrlEntity urlEntity = new UrlEntity();
             urlEntity.setLongUrl(entity.getLongUrl());
-//            System.out.println(entity.getLongUrl());
+            urlEntity.setEmail(entity.getEmail());
 
 //            expiration time ko lagi ali pachhi kaam garchhu.
 //        urlEntity.setExpirationTime(Instant.ofEpochSecond(entity.getExpirationTime().getSeconds(), entity.getExpirationTime().getNanos()));
@@ -45,12 +44,11 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
 //            - url validation
 
 //          1. generating hash of long url using sha-256
-            byte[] hash = new byte[0]; // creating empty array of byte.
+            byte[] hash; // creating empty array of byte.
             StringBuilder hexOfHash = new StringBuilder();
             String short_url = "";
             if(!urlEntity.getLongUrl().isEmpty()){
                 String long_url = urlEntity.getLongUrl();
-//            System.out.println(long_url);
                 try{
                     MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
                     hash = messageDigest.digest(long_url.getBytes(StandardCharsets.UTF_8));
@@ -103,6 +101,7 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
 
     @Override
     public void getUrl(GetUrlRequest getUrlRequest, StreamObserver<GetUrlResponse> responseObserver) {
+        System.out.println("I am here at getUrl");
 //         user le short url browser ma halesi teslai resolve garera long url dini kaam yesko ho.
 //        System.out.println(getUrlRequest.getShortUrl());
         if(!getUrlRequest.getShortUrl().isEmpty()){
@@ -137,7 +136,35 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
         try{
             return urlRepository.findByShortUrl(shortUrl).getLongUrl();
         } catch (EmptyResultException emptyResultException){
-            return "Long URL not found in database";
+            System.out.println("Oh no! Long URL not found in database");
+            return "";
+        }
+    }
+
+    @Override
+    public void getAllUrlsOfUser(UserEmail userEmail, StreamObserver<GetShortUrlsResponse> responseObserver) {
+        System.out.println(userEmail.getEmail());
+        String email = userEmail.getEmail();
+        if(!email.isEmpty()){
+            try{
+                List<UrlEntity> urlEntities = urlRepository.findByEmail(email);
+                for (UrlEntity urlEntity : urlEntities) {
+                    String shortUrl = urlEntity.getShortUrl();
+                    GetShortUrlsResponse getShortUrlsResponse = GetShortUrlsResponse.newBuilder()
+                            .setShortUrl(shortUrl)
+                            .build();
+
+                    responseObserver.onNext(getShortUrlsResponse);  // Send back each response
+                }
+                responseObserver.onCompleted();  // Signal the end of the stream
+            } catch (EmptyResultException emptyResultException){
+                Status status = Status.NOT_FOUND.withDescription("Long URL not found in database");
+                responseObserver.onError(status.asRuntimeException());
+            }
+        }
+        else{
+            Status status = Status.INTERNAL.withDescription("No email associated provided");
+            responseObserver.onError(status.asRuntimeException());
         }
     }
 
@@ -148,17 +175,27 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
 
     @Override
     public void deleteUrl(DeleteUrlRequest deleteUrlRequest, StreamObserver<DeleteUrlResponse> responseObserver) {
-        if(!deleteUrlRequest.getShortUrl().isEmpty()){
+        if(!deleteUrlRequest.getShortUrl().isEmpty() && !deleteUrlRequest.getEmail().isEmpty()){
+            String shortUrl = deleteUrlRequest.getShortUrl().substring(15);
             try{
-                if(!urlRepository.findByShortUrl(deleteUrlRequest.getShortUrl()).getLongUrl().isEmpty()){
-                    try{
-                        urlRepository.deleteByShortUrl(deleteUrlRequest.getShortUrl());
-                        responseObserver.onNext(DeleteUrlResponse.newBuilder().setStatus("Data Deleted Successfully").build());
-                        responseObserver.onCompleted();
-                    } catch (Exception e){
-                        Status status = Status.UNKNOWN.withDescription("Some unknown error occurred while trying to delete data");
+                if(!urlRepository.findByShortUrl(shortUrl).getLongUrl().isEmpty()){
+
+                    if(!urlRepository.findByEmail(deleteUrlRequest.getEmail()).isEmpty()){
+                        try{
+                            urlRepository.deleteByEmailAndShortUrl(deleteUrlRequest.getEmail(), shortUrl);
+                            responseObserver.onNext(DeleteUrlResponse.newBuilder().setStatus("Data Deleted Successfully").build());
+                            responseObserver.onCompleted();
+                        } catch (Exception e){
+                            Status status = Status.UNKNOWN.withDescription("Some unknown error occurred while trying to delete data");
+                            responseObserver.onError(status.asRuntimeException());
+                        }
+                    }
+                    else{
+                        Status status = Status.NOT_FOUND.withDescription("Given email is not present in Database");
                         responseObserver.onError(status.asRuntimeException());
                     }
+
+
                 }
 
             }
@@ -168,7 +205,7 @@ public class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
             }
         }
         else{
-            Status status = Status.INTERNAL.withDescription("Empty URL provided");
+            Status status = Status.INTERNAL.withDescription("Empty URL provided or Email provided");
             responseObserver.onError(status.asRuntimeException());
         }
     }
